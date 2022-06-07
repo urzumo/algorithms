1dfa384544a5493943ea15c01a8cab88c4193374
Anton Maksakov <92244169+urzumo@users.noreply.github.com>
2022-06-07 10:32:16 +0300

    Update 02_find_dups_in_list.py

--- algorithms/02_find_dups_in_list.py
+++ algorithms/02_find_dups_in_list.py
@@ -1,7 +1,10 @@
 # поиск и вывод дубликатов в листе
 # возвращает список дубликатов
 def find_dupes(data):
+    # формируем пустые множества 
     dups, seen = set(), set()
+    
+    # пробегаем по списку
     for element in data:
         if element in seen:
             dups.add(element)
